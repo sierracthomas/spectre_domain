3999c0dd16e0a65f50ebf41b96c3f724ea9f54d9
sierra.c.thomas@gmail.com <sierra.c.thomas@gmail.com>
2020-07-03 10:25:38 -0700

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Make softlink to ExportCoordinates executable - make sure this is compiled in your
 # SpECTRE repo
 
-export SPECTRE_BUILD_DIRECTORY = path/to/build/directory
+export SPECTRE_BUILD_DIRECTORY=/home/sierra/new_spectre/spectre/build
 #export SPEC = path/to/spec
 
 cd spectre_start_domain
@@ -11,8 +11,8 @@ ln -s ${SPECTRE_BUILD_DIRECTORY}/bin/ExportCoordinates3D .
 
 # Make a SpEC interpolation directory and copy over SpEC
 
-mkdir ../spec_interp && cd ../spec_interp
-cp /home/geoffrey/aps2020/after_aps/import/Domain5b/spec_interp/* .
+cd ..
+cp -r /home/geoffrey/aps2020/after_aps/import/Domain5b/spec_interp .
 
 # Make old/ directories for the cleanup script
 
