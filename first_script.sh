63afede526378c5f56ce9e1bcd4deda8b94cb66a
sierra.c.thomas@gmail.com <sierra.c.thomas@gmail.com>
2020-07-03 14:15:55 -0700

    Formatting

--- first_script.sh
+++ first_script.sh
@@ -5,10 +5,13 @@ export DOMAIN_INPUT=${PWD}/BBH_working.yaml
 
 cd spectre_start_domain/
 module load ohpc
+
 # Make VolumeData0.h5 file based on the input file
 singularity exec /opt/ohpc/pub/containers/spectre_ocean.sif ./ExportCoordinates3D --input-file ${DOMAIN_INPUT}
+
 # Output VolumeData0.h5 into a txt file that is readable by SpEC
 singularity exec /opt/ohpc/pub/containers/spectre_ocean.sif python convert_spectre_spec_coords.py --spectre-points-filename VolumeData0.h5 --output-spec-points-filename PointsList.txt
 cd ../spec_interp
+
 # Run SpEC job to interpolate data onto the domain
 ./StartJob.sh
