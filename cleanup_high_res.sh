f453cf4c7bd8ede256577d95ce567ba82d19153d
sierra.c.thomas@gmail.com <sierra.c.thomas@gmail.com>
2021-01-12 13:41:46 -0800

    Add cleanup script for high res runs

---
+++ cleanup_high_res.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# cleanup script from previous domain tests. Must have a spectre_start_domain/old/ directory
+cd spectre_start_domain
+mv PointsList.txt old/
+mv VolumeData0.h5 old/
+cd ../spec_interp
+rm -r Lev5
+cd ../spectre_interp
+rm BH_ID_VolumeData0.h5
+rm /home/sierra/gw_spectre/initial_data_interp/spectre_interp/ID_BBH_VolumeData0.h5
+cd ../spectre_constraints
+rm -r Run
+rm spectre.std*
+mkdir ../Old/
+mv ../Interps/ ../Old/.
+cd ..
+rm /home/sierra/gw_spectre/initial_data_interp/spectre_constraints/BH_ID_VolumeData0.h5
--- first_script.sh
+++ first_script.sh
@@ -9,9 +9,6 @@ spectre_load_modules
 # Make VolumeData0.h5 file based on the input file
 ${SPECTRE_BUILD_DIR}/bin/ExportCoordinates3D --input-file BBH_working.yaml
 
-module purge
-module load ohpc
-
 # Output VolumeData0.h5 into a txt file that is readable by SpEC
 python convert_spectre_spec_coords.py --spectre-points-filename VolumeData0.h5 --output-spec-points-filename PointsList.txt
 cd ../spec_interp
