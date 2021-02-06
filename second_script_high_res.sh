16a6996695744a33f6eb1ed555f11239d35b1d50
sierra.c.thomas@gmail.com <sierra.c.thomas@gmail.com>
2021-02-05 21:13:00 -0800

    Generalize scripts to remove legends from .dat files

--- second_script_high_res.sh
+++ second_script_high_res.sh
@@ -6,13 +6,11 @@ spectre_load_modules
 
 cd Interps/
 # Get rid of legends at the top of each file except first
-sed -i '1,3d' 2.dat
-sed -i '1,3d' 3.dat
-sed -i '1,3d' 4.dat
-sed -i '1,3d' 5.dat
-sed -i '1,3d' 6.dat
-sed -i '1,3d' 7.dat
-sed -i '1,3d' 8.dat
+num=2
+for i in *.dat; do 
+    sed -i '1,3d' "$(printf $num).dat"
+    ((num++))
+done 
 
 # Join files together
 cat 1.dat 2.dat 3.dat 4.dat 5.dat 6.dat 7.dat 8.dat > ../spectre_interp/Interp.dat
